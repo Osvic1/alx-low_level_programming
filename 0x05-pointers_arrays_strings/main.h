4a0ab609e3485fb36649411b0e35a1a5bb0fff24
Osvic1 <timothyv952@gmail.com>
2022-10-24 06:46:00 -0700

    int _putchar(char c); void reset_to_98(int *n); void swap_int(int *a, int *b); int _strlen(char *s); void _puts(char *str); void print_rev(char *s); void rev_string(char *s); void puts2(char *str); void puts_half(char *str); void print_array(int *a, int n); char *_strcpy(char *dest, char *src); int _atoi(char *s);

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,11 +8,9 @@
  *       used in the 0x04-pointers_arrays_strings directory.
  */
 
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
