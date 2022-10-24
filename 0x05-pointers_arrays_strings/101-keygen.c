d73b3e6856c7f05773df7beff69f66f4845d78e2
Osvic1 <timothyv952@gmail.com>
2022-10-24 14:39:10 -0700

    Random key-gen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+/*
+ * File: 101-keygen.c
+ * Auth: Timothy Victor
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random valid passwords for the
+ *        program 101-crackme.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+int sum;
+	char c;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
+
+putchar('\n');
+	return (0);
+}
