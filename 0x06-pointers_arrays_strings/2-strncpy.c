cba2ce77b9567591c985f137fb1a12be0d28a485
Osvic1 <timothyv952@gmail.com>
2022-10-26 07:09:48 -0700

    function to copy strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,37 @@
+/*
+ * File: 2-strncpy.c
+ * Auth: Timothy Victor
+ */
+
+#include "main.h"
+
+/**
+ * _strncpy - Copies at most an inputted number
+ *            of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
