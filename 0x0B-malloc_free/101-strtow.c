f5c1c3d38af0d6fc74ef150028b4a3ad2d1c247d
Osvic1 <timothyv952@gmail.com>
2022-11-07 10:01:33 -0800

    function that splits a string into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,20 @@
+/*
+ * File: 101-strtow.c
+ * Auth: Timothy Victor
+ */
+
+#include "main.h"
+
+/**
+ * strtow - split to words
+ * @str:
+ *
+ * Return: 0
+ */
+char **strtow(char *str)
+{
+	char **wordn;
+	char *word;
+	int i, j;
+
+}
