1637ec2f2ad05362afbc3d1ecb25b2e284127ea5
Osvic1 <timothyv952@gmail.com>
2022-11-14 10:08:57 -0800

    Hot fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MACRO
-#define ABS(x) (((x) < 0) ? (x * -1) : (x))
+#ifndef ABS_H_
+#define ABS_H_
+
+#define ABS(x) ((x) >= 0 ? (x) : (x) * (-1))
+
 #endif
