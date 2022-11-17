73b5abe3c4c998a5499794c1b8d6f6f3f19789b5
Osvic1 <timothyv952@gmail.com>
2022-11-17 06:02:28 -0800

    Variadic function to print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(nums, n);
 
-	for (i = 0;i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(nums, int));
 
