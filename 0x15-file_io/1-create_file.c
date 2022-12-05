18a1035561b7c414ff575c25e6bb28633d88c1cc
Osvic1 <timothyv952@gmail.com>
2022-12-05 08:36:24 -0800

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,27 +12,25 @@
  * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
-{
-	int fd, chars_written, str_len = 0;
 
+{
+	int file, len, closed;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	len = 0;
 	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	if (fd == -1)
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (file == -1)
 		return (-1);
-
 	if (text_content != NULL)
 	{
-		while (text_content[str_len])
-			str_len++;
-
-		chars_written = write(fd, text_content, str_len);
-
-		if (chars_written != str_len)
-			return (-1);
+		while (text_content[len] != '\0')
+			len++;
+		write(file, text_content, len);
 	}
-	close(fd);
+	closed = close(file);
+	if (closed == -1)
+		return (-1);
 	return (1);
 }
