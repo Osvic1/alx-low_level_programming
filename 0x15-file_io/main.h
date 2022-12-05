e32b08d80a12a0fd357572be10dc0830ac117b3d
Osvic1 <timothyv952@gmail.com>
2022-12-05 07:47:16 -0800

    function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif
