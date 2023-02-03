b4623abd021792763acd90b4f50aa761f644b436
Osvic1 <timothyv952@gmail.com>
2023-02-02 16:22:44 -0800

    a function that retrieves a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,17 +8,26 @@
  * Return:the value associated with the element,or NULL if key couldnt be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
-{
-	unsigned long int index;
-	hash_node_t *node;
+{ unsigned long int index; /* Index of hash table */
+hash_node_t *node; /* Node to iterate linked list */
 
-	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index];
-	while (node != NULL)
-	{
-		if (strcmp(node->key, key) == 0)
-			return (node->value);
-		node = node->next;
-	}
-	return (NULL);
+/* Return NULL if the table, key or key value is invalid */
+if (!ht || !key || !*key)
+return (NULL);
+/* Get index for the key */
+index = key_index((unsigned char *)key, ht->size);
+/* Return NULL if index is out of bounds */
+if (index >= ht->size)
+return (NULL);
+/* Get the node at index */
+node = ht->array[index];
+/* Iterate linked list and return value if key matches */
+while (node)
+{
+if (!strcmp(node->key, key))
+return (node->value);
+node = node->next;
+}
+/* Return NULL if key was not found */
+return (NULL);
 }
