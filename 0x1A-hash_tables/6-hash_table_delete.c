82fe348ae7e4eef5f981bfc96de2261b8bb5b76e
Osvic1 <timothyv952@gmail.com>
2023-02-03 06:33:25 -0800

    a function that deletes a hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,41 +1,30 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_delete - Deletes a hash table.
-* @ht: A pointer to a hash table
-*/
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
 void hash_table_delete(hash_table_t *ht)
 {
-/* Check if the hash table is already NULL */
-if (ht == NULL)
-{
-return;
-}
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-/* Loop through all buckets in the array */
-unsigned long int i;
-for (i = 0; i < ht->size; i++)
-{
-/* Get the head of the linked list in the current bucket */
-hash_node_t *node = ht->array[i];
-
-/* Free all nodes in the linked list */
-while (node != NULL)
-{
-hash_node_t *tmp = node->next;
-
-/* Free the key, value, and node */
-free(node->key);
-free(node->value);
-free(node);
-
-node = tmp;
-}
-}
-
-/* Free the array of buckets */
-free(ht->array);
-
-/* Free the hash table */
-free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
 }
